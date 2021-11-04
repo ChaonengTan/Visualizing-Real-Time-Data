adc06e587918038ded82e09d729475fe01b83a8e
Chaoneng Tan <chaonengtan@Chaonengs-MacBook-Air.local>
2021-11-03 23:44:24 -0700

    RYG
    Red, Yellow, and Blue based on frequency intensity

--- yes.js
+++ yes.js
@@ -15,6 +15,7 @@ export default function render(frequencyArray, ctx, width, height) {
         ctx.moveTo(step * i-1, maxModifier(frequencyArray[i-1])/2 + height/2)
         // move to current value
         ctx.lineTo(step * i, -maxModifier(val)/2 + height/2)
+        ctx.strokeStyle = `hsl(${90-90 / height * val}, 100%, 50%)`
         ctx.stroke()
 	})
 }

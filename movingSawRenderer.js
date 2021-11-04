e0962b13b3fcd92d4665452d2a54f708b8b8539b
Chaoneng Tan <chaonengtan@Chaonengs-MacBook-Air.local>
2021-11-03 23:15:50 -0700

    width to step

--- movingSawRenderer.js
+++ movingSawRenderer.js
@@ -4,5 +4,8 @@ export default function render(frequencyArray, ctx, width, height) {
 	ctx.fillRect(0, 0, width, height)
 	ctx.fill()
 
+    // px per step
+	const step = width / frequencyArray.length
+
     
 }

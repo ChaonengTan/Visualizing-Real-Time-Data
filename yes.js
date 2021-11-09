d871bd63c4db9815185b5ab14fc0fc4142bf4c26
Chaoneng Tan <chaonengtan@Chaonengs-MacBook-Air.local>
2021-11-08 17:27:58 -0800

    seperateBase

--- yes.js
+++ yes.js
@@ -5,12 +5,22 @@ export default function render(frequencyArray, ctx, width, height) {
 	ctx.fill()
 
     // px per step
-	const step = width / frequencyArray.length
+    const base = 128
+	const step = width / (frequencyArray.length-base)
+    const bW = width / base
 
     // draws visualizer
     const maxModifier = x => x/255*height
 	frequencyArray.forEach((val, i) => {
         ctx.beginPath()
+        if(i<base) {
+            ctx.moveTo(bW * i-1, maxModifier(frequencyArray[i-1])/2 + height)
+            ctx.lineTo(bW * i, -maxModifier(val)/2 + height)
+            ctx.moveTo(width - bW * i-1, maxModifier(frequencyArray[i-1])/2 + height)
+            ctx.lineTo(width - bW * i, -maxModifier(val)/2 + height)
+            ctx.closePath()
+        }
+        
         // previous value
         ctx.moveTo(step * i-1, maxModifier(frequencyArray[i-1])/2 + height/2)
         // move to current value
